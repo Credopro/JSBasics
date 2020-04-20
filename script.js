49ab91a02a82220de64ffc72b3f57bb087af8cea
Credo <credopro@gmail.com>
2020-04-20 13:51:21 +0300

    Це комміт створення

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+console.log(4/0);
+
+console.log('string'*9);
+
+let something;
+console.log (something);
+let persone = {
+  name: "Jone",
+  age: 25,
+  isMarried: false
+};
+
+console.log(persone.name);
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("Hello word");
+
+confirm("Are you here?");
